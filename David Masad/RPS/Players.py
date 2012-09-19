00548a65e38a0d211ad06f2d98369a8ab0aaa08c
David Masad <david.masad@gmail.com>
2012-09-19 16:47:38 -0400

    Added player types
    Added SequencePlayer, MLPlayer and HumanPlayer

--- David Masad/RPS/Players.py
+++ David Masad/RPS/Players.py
@@ -85,8 +85,9 @@ class RandomPlayer(Player):
         Instantiate a RandomPlayer player.
 
         Args:
-            action_list: The list of actions from the game object
+            game: A game object.
         '''
+
         self.game = game
 
     def go(self):
@@ -101,5 +102,94 @@ class RandomPlayer(Player):
         Doesn't care, won't change.
         '''
         pass
+
+class SequencePlayer(Player):
+    '''
+    A player who plays a fixed sequence of actions.
+    '''
+
+    def __init__(self, game, cycle_length = 3):
+        '''
+        Args:
+            cycle_length: The lenght of the player's fixed action sequence.
+        '''
+        self.game = game
+        self.cycle = [random.choice(self.game.action_list) for i in range(cycle_length)]
+        self.counter = 0
+
+    def go(self):
+        '''
+        Pick the next action in the sequence. 
+        '''
+        action = self.cycle[self.counter % len(self.cycle)]
+        self.counter += 1 
+        return action
+
+    def receive_score(self, score):
+        '''
+        Don't care, won't change.
+        '''
+        pass
+
+
+class MLPlayer(Player):
+    '''
+    A player that uses simple learning to make the next move.
+    '''
+
+    def __init__(self, game):
+        self.game = game
+        self.action_outcomes = {}
+        for action in self.game.action_list:
+            self.action_outcomes[action] = 1
+
+    def go(self):
+        '''
+        Choose an action based on the assigned weights
+        '''
+        action_list = []
+        for action, weight in self.action_outcomes.items():
+            for _ in range(weight): action_list.append(action)
+        action = random.choice(action_list)
+        self.last_action = action
+        return action
+    
+    def receive_score(self, score):
+        '''
+        Modify action weights by score
+        '''
+        self.action_outcomes[self.last_action] += score
+
+class HumanPlayer(Player):
+    '''
+    Interface for letting a human play.
+    '''
+
+    def __init__(self, game):
+        self.game = game
+        print "Welcome New Player"
+        print "The available actions are: "
+        print self.game.action_list
+        print "The payoff matrix is: "
+        for key, payoff in self.game.payoff_matrix.items():
+            print "\t" + str(key) + ": " + str(payoff)
+
+    def go(self):
+        print "Your move!"
+        while True:
+            move = raw_input("Enter your action\n")
+            if move not in self.game.action_list:
+                print "Invalid move. Available moves are: "
+                print self.game.action_list
+            else:
+                break
+        return move
+
+    def receive_score(self, score):
+        print "Your score for the round is " + str(score)
+
+
+
+
 
 
